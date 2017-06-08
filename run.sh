296ae61f2bd72796e80da359c40cfdc4fa6359cb
Frangel Hernandez <fhernandez@avathartech.com>
2017-06-08 19:29:20 -0400

    waaaaaao

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ rm -rf output
 hadoopStream=/usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.8.0.jar
 mapper=groovy/map/mapTiendaMonto.groovy
 reducer=groovy/map/mapTiendaMonto.groovy
-inputFile=input
+inputFile=./input
 outputFile=./output
-hadoop jar $hadoopStream -input ./t -output $outputFile -mapper "$mapper" -reducer "$reducer"
+hadoop jar $hadoopStream -input ./inputFile -output $outputFile -mapper "$mapper" -reducer "$reducer"
 # echo "hadoop jar $hadoopStream -input ./t -output $outputFile -mapper "$mapper" -reducer "$reducer""
